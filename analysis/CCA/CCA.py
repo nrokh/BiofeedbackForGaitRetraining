a763d5de47bc8e064bd0caff1db5ab00983ba140
Nataliya Rokhmanova <n.rokhmanova@gmail.com>
2024-12-09 16:58:04 -0600

    add CCA

---
+++ analysis/CCA/CCA.py
@@ -0,0 +1,199 @@
+import numpy as np
+import pandas as pd
+import os
+import tkinter as tk
+from tkinter import filedialog
+from sklearn.cross_decomposition import CCA
+from sklearn.linear_model import Lasso
+import matplotlib.pyplot as plt
+from statsmodels.multivariate.cancorr import CanCorr
+from sklearn.model_selection import KFold
+from sklearn.model_selection import LeaveOneOut
+from sklearn.metrics import mean_squared_error
+import scipy
+
+# a. Get the desired directory to load/save the data
+root = tk.Tk()
+root.withdraw() 
+directory = filedialog.askdirectory()
+
+# b. set up
+n_features_in = 11  # input
+n_features_out = 3  # output
+alpha = 0.1         # Lasso
+np.set_printoptions(suppress=True) # suppress scientific notation
+
+# c. load inputs and outputs
+# i. INPUTS:
+
+    # height
+in_height_file = os.path.normpath(os.path.join(directory,'features\\in_height.csv'))
+in_height = np.abs(np.genfromtxt(in_height_file, delimiter=','))
+
+    # weight
+in_weight_file = os.path.normpath(os.path.join(directory,'features\\in_weight.csv'))
+in_weight = np.abs(np.genfromtxt(in_weight_file, delimiter=','))
+
+    # sex
+in_isFemale_file = os.path.normpath(os.path.join(directory,'features\\in_isFemale.csv'))
+in_isFemale = np.abs(np.genfromtxt(in_isFemale_file, delimiter=','))
+
+    # vibration test accuracy
+in_vbtest_file = os.path.normpath(os.path.join(directory, 'features\\in_vbtest.csv'))
+in_vbtest = np.genfromtxt(in_vbtest_file, delimiter=',')
+
+    # proprioception test accuracy
+in_proprio_in_file = os.path.normpath(os.path.join(directory,'features\\in_proprio_in.csv'))
+in_proprio_in = np.abs(np.genfromtxt(in_proprio_in_file, delimiter=','))
+
+in_proprio_out_file = os.path.normpath(os.path.join(directory,'features\\in_proprio_out.csv'))
+in_proprio_out = np.abs(np.genfromtxt(in_proprio_out_file, delimiter=','))
+
+    # responsiveness to error
+in_resp_file = os.path.normpath(os.path.join(directory, 'features\\in_resp.csv'))
+in_resp = np.genfromtxt(in_resp_file, delimiter=',')
+
+    # dynamic range of motion during gait
+in_ROM_in_file = os.path.normpath(os.path.join(directory, 'features\\in_ROM_in.csv'))
+in_ROM_in = np.genfromtxt(in_ROM_in_file, delimiter=',')    
+
+in_ROM_out_file = os.path.normpath(os.path.join(directory, 'features\\in_ROM_out.csv'))
+in_ROM_out = np.genfromtxt(in_ROM_out_file, delimiter=',')  
+
+    # baseline FPA
+in_bFPA_file = os.path.normpath(os.path.join(directory, 'features\\in_bFPA.csv'))
+in_bFPA = np.genfromtxt(in_bFPA_file, delimiter=',')
+
+    # feedback condition (binary, with NF as 0)
+feedbackCond_csv_file = os.path.normpath(os.path.join(directory, 'feedbackGroups.csv'))
+feedbackCond_file = pd.read_csv(feedbackCond_csv_file)
+in_cond_fb = np.zeros((36,))
+for i in range(1,37):
+        if feedbackCond_file.cond[i-1] == 1 or feedbackCond_file.cond[i-1] == 2:
+                in_cond_fb[i-1] = 1
+
+# ii. OUTPUTS:
+    # RMSE
+out_RMSE_file = os.path.normpath(os.path.join(directory, 'features\\out_RMSE.csv'))
+out_RMSE = np.genfromtxt(out_RMSE_file, delimiter=',')
+
+    # error ratio (note: error_in = 1 - error_out)
+out_errRatio_out_file = os.path.normpath(os.path.join(directory,'features\\out_errRatio_out.csv'))
+out_errRatio_out = np.abs(np.genfromtxt(out_errRatio_out_file, delimiter=','))
+
+
+# d. assemble inputs and outputs into single arrays:
+X = np.stack((in_height[1:], in_weight[1:], in_isFemale[1:], 
+              in_vbtest[1:], in_proprio_in[1:], in_proprio_out[1:], in_resp[1:,4],
+              np.abs(in_ROM_in[1:]), np.abs(in_ROM_out[1:]),
+              in_bFPA[1:], in_cond_fb), axis=1) 
+Y = np.stack((out_RMSE[1:,4], out_RMSE[1:,5], out_errRatio_out[1:,5]), axis=1)
+
+# e. run Lasso regression
+lasso = Lasso(alpha=alpha)
+lasso.fit(X, Y)
+print(lasso.coef_)
+selected_features = np.abs(lasso.coef_) > alpha
+print("Selected features:", selected_features)
+
+# f. run CCA on selected features:
+X = np.stack((in_proprio_in[1:], in_bFPA[1:], in_cond_fb, in_resp[1:,4]), axis=1) # shape = 36xN
+Y = np.stack((out_RMSE[1:,4], out_RMSE[1:,5], out_errRatio_out[1:,5]), axis=1)
+
+# i. leave-one-out pre-validation
+SF_rows = np.where(feedbackCond_file.cond == 1)[0]
+TF_rows = np.where(feedbackCond_file.cond == 2)[0]
+NF_rows = np.where(feedbackCond_file.cond == 0)[0]
+
+Y_pred = np.zeros_like(Y)
+X_pred = np.zeros_like(Y)
+loo = LeaveOneOut()
+
+for train_index, test_index in loo.split(X):
+    X_train, X_test = X[train_index], X[test_index]
+    Y_train, Y_test = Y[train_index], Y[test_index]
+
+    cca = CCA(n_components=min(X.shape[1], Y.shape[1]))
+    cca.fit(X_train, Y_train)
+
+    X_pred[test_index], Y_pred[test_index] = cca.transform(X_test, Y_test)
+
+results = []
+for i in range(2):
+    
+    plt.figure(figsize=(6, 6))
+    plt.scatter(X_pred[SF_rows, i], Y_pred[SF_rows, i], alpha=0.7, color = '#0f4c5c', label = 'SF', s = 60)
+    plt.scatter(X_pred[TF_rows, i], Y_pred[TF_rows, i], alpha=0.7, color = '#5f0f40', label = 'TF', s = 60)
+    plt.scatter(X_pred[NF_rows, i], Y_pred[NF_rows, i], alpha=0.7, color = '#e36414', label = 'NF', s = 60)
+    plt.title("Canonical Variate")
+    plt.xlabel("X index")
+    plt.ylabel("Y index")
+    plt.ylim([-2.2, 2])
+    plt.xlim([-2.5, 1.7])
+    plt.legend()
+    plt.show()
+
+    r, p = scipy.stats.pearsonr(X_pred[:, i], Y_pred[:, i])
+    
+    # calculate F-value
+    n = len(X_pred)
+    dof = n - 2
+    f = (r**2 * dof) / (1 - r**2)
+    
+    results.append({
+        'Component': i + 1,
+        'Pearson R': r,
+        'F-value': f,
+        'p-value': p
+    })
+        
+# print pre-validation results
+for result in results:
+    print(f"Canonical Component {result['Component']}:")
+    print(f"  Pearson's R: {result['Pearson R']:.4f}")
+    print(f"  F-value: {result['F-value']:.4f}")
+    print(f"  p-value: {result['p-value']:.4f}")
+    print()
+
+# ii. compute individual feature loadings:
+n_components = min(n_features_in, n_features_out)
+cca = CCA(n_components=n_components)
+X_c, Y_c = cca.fit_transform(X, Y)
+
+print("\nX loadings:")
+print(cca.x_loadings_)
+print("\nY loadings:")
+print(cca.y_loadings_)
+# each column = the x loadings for each variate;
+# first column (first variate) shows the strength of contributions of each input feature to that variate
+
+# manually calculate p-values for the loadings between the original variables and the canonical variates:
+n = X.shape[0]  # sample size
+
+# get standard errors
+se_X = np.sqrt((1 - cca.x_loadings_**2) / (n - 2))
+se_Y = np.sqrt((1 - cca.y_loadings_**2) / (n - 2))
+
+# get t-values
+t_X = cca.x_loadings_ / se_X
+t_Y = cca.y_loadings_ / se_Y
+
+# get p-values (two-tailed test)
+p_X = 2 * (1 - scipy.stats.t.cdf(np.abs(t_X), n-2))
+p_Y = 2 * (1 - scipy.stats.t.cdf(np.abs(t_Y), n-2))
+
+
+print("\nP-values for X loadings:")
+print(str(p_X))
+
+print("\nP-values for Y loadings:")
+print(str(p_Y))
+
+# overall model summary:
+cca_model = CanCorr(X,Y)
+results = cca_model.corr_test()
+
+print("\nSummary:")
+print(results.summary())
+
+
---
+++ analysis/main/primaryMeasures.py
@@ -0,0 +1,302 @@
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import tkinter as tk
+from tkinter import filedialog
+from scipy import stats
+from matplotlib.colors import to_rgba
+import ptitprince as pt 
+import seaborn as sns
+
+def calculate_responsiveness(input_FPA, targetFPA):
+    incorrect_steps = 0
+    resp_tally = 0
+    
+    for step in range(len(input_FPA) - 1):
+        current_fpa = input_FPA.iloc[step, 2]
+        next_fpa = input_FPA.iloc[step + 1, 2]
+        
+        if current_fpa < targetFPA - 2:
+            incorrect_steps += 1
+            if next_fpa > current_fpa:
+                resp_tally += 1
+        
+        elif current_fpa > targetFPA + 2:
+            incorrect_steps += 1
+            if next_fpa < current_fpa:
+                resp_tally += 1
+    
+    store_resp = resp_tally / incorrect_steps if incorrect_steps > 0 else 0
+    
+    return store_resp
+
+# 0. Create general vars
+subs_tot = 36
+# Accuracy
+store_inRangePercent = np.zeros((subs_tot, 6)) 
+store_inRangeNFPercent = np.zeros((subs_tot, 6)) 
+# RMSE
+store_RMSE = np.zeros((subs_tot, 6))
+store_C_RMSE = np.zeros((subs_tot, 6))
+# Responsiveness
+store_resp = np.zeros((subs_tot, 6))
+
+# 1. Load data
+# a. Get the desired directory 
+root = tk.Tk()
+root.withdraw() 
+directory = filedialog.askdirectory()
+
+# b. Load feedback condition ID (1:SF, 2:TF, 0:NF)
+feedbackCond_csv_file = os.path.normpath(os.path.join(directory, 'feedbackGroups.csv'))
+feedbackCond_file = pd.read_csv(feedbackCond_csv_file)
+
+# c. Iterate through subjects 
+for subject in range(1,37):
+    print('----------------Starting analysis for subject ' + str(subject) + '--------------------')
+    if feedbackCond_file.cond[subject-1] == 1:
+        print('----------------CONDITION: SCALED FEEDBACK------------')
+    elif feedbackCond_file.cond[subject-1] == 2:
+        print('----------------CONDITION: TRINARY FEEDBACK------------')
+    elif feedbackCond_file.cond[subject-1] == 0:
+        print('----------------CONDITION: NO FEEDBACK------------')
+    else:
+        print('!!!--------ERROR: check feedback condition file?---------!!!')
+
+    if subject < 10:
+        # Load FPAs:
+        baseline_csv_file = os.path.normpath(os.path.join(directory, 's0' + str(subject)  + '\\s0' + str(subject) + '_baseline_meanFPA.csv'))
+        baselineFPA = pd.read_csv(baseline_csv_file)
+
+        nf_csv_file = os.path.normpath(os.path.join(directory, 's0' + str(subject)  + '\\s0' + str(subject) + '_noFB_meanFPA.csv'))
+        nfFPA = pd.read_csv(nf_csv_file)
+
+        toein1_csv_file = os.path.normpath(os.path.join(directory, 's0' + str(subject)  + '\\s0' + str(subject) + '_meanFPA_1.csv'))
+        toein1FPA = pd.read_csv(toein1_csv_file)
+
+        toein2_csv_file = os.path.normpath(os.path.join(directory, 's0' + str(subject)  + '\\s0' + str(subject) + '_meanFPA_2.csv'))
+        toein2FPA = pd.read_csv(toein2_csv_file)
+
+        toein3_csv_file = os.path.normpath(os.path.join(directory, 's0' + str(subject)  + '\\s0' + str(subject) + '_meanFPA_3.csv'))
+        toein3FPA = pd.read_csv(toein3_csv_file)
+
+        toein4_csv_file = os.path.normpath(os.path.join(directory, 's0' + str(subject)  + '\\s0' + str(subject) + '_meanFPA_4.csv'))
+        toein4FPA = pd.read_csv(toein4_csv_file)
+
+        ret_csv_file = os.path.normpath(os.path.join(directory, 's0' + str(subject)  + '\\s0' + str(subject) + '_retention_meanFPA.csv'))
+        retFPA = pd.read_csv(ret_csv_file)
+
+        fullFPA = pd.concat([baselineFPA, toein1FPA, toein2FPA, toein3FPA, toein4FPA, retFPA])
+    else:
+        baseline_csv_file = os.path.normpath(os.path.join(directory, 's' + str(subject)  + '\\s' + str(subject) + '_baseline_meanFPA.csv'))
+        baselineFPA = pd.read_csv(baseline_csv_file)
+
+        nf_csv_file = os.path.normpath(os.path.join(directory, 's' + str(subject)  + '\\s' + str(subject) + '_noFB_meanFPA.csv'))
+        nfFPA = pd.read_csv(nf_csv_file)
+
+        toein1_csv_file = os.path.normpath(os.path.join(directory, 's' + str(subject)  + '\\s' + str(subject) + '_meanFPA_1.csv'))
+        toein1FPA = pd.read_csv(toein1_csv_file)
+
+        toein2_csv_file = os.path.normpath(os.path.join(directory, 's' + str(subject)  + '\\s' + str(subject) + '_meanFPA_2.csv'))
+        toein2FPA = pd.read_csv(toein2_csv_file)
+
+        toein3_csv_file = os.path.normpath(os.path.join(directory, 's' + str(subject)  + '\\s' + str(subject) + '_meanFPA_3.csv'))
+        toein3FPA = pd.read_csv(toein3_csv_file)
+
+        toein4_csv_file = os.path.normpath(os.path.join(directory, 's' + str(subject)  + '\\s' + str(subject) + '_meanFPA_4.csv'))
+        toein4FPA = pd.read_csv(toein4_csv_file)
+
+        ret_csv_file = os.path.normpath(os.path.join(directory, 's' + str(subject)  + '\\s' + str(subject) + '_retention_meanFPA.csv'))
+        retFPA = pd.read_csv(ret_csv_file)
+
+        fullFPA = pd.concat([baselineFPA, toein1FPA, toein2FPA, toein3FPA, toein4FPA, retFPA])
+
+    # d. Compute baseline FPA
+    bFPA_deg = np.mean(baselineFPA.iloc[:,2])
+    print('Baseline FPA was: ' + str(bFPA_deg) + '(' + str(np.std(baselineFPA.iloc[:,2])) + ')')
+
+    # e. Compute accuracy
+    targetFPA = bFPA_deg-10
+
+    # i. nominal condition
+    inRange_nf = 100*len(nfFPA[(nfFPA.iloc[:,2]<targetFPA+2) & (nfFPA.iloc[:,2]>targetFPA-2)])/len(nfFPA)
+    print('percent steps in range during NF: ' + str(inRange_nf))
+
+    # ii. toe-in trials
+    inRange_t1 = 100 * len(pd.concat([toein1FPA.iloc[:80], toein1FPA.iloc[121:]])[(pd.concat([toein1FPA.iloc[:80], toein1FPA.iloc[121:]]).iloc[:, 2] < targetFPA + 2) & (pd.concat([toein1FPA.iloc[:80], toein1FPA.iloc[121:]]).iloc[:, 2] > targetFPA - 2)]) / (len(toein1FPA) - 41)
+    print('percent steps in range during toe-in 1: ' + str(inRange_t1))
+    inRange_t2 = 100 * len(pd.concat([toein2FPA.iloc[:80], toein2FPA.iloc[121:]])[(pd.concat([toein2FPA.iloc[:80], toein2FPA.iloc[121:]]).iloc[:, 2] < targetFPA + 2) & (pd.concat([toein2FPA.iloc[:80], toein2FPA.iloc[121:]]).iloc[:, 2] > targetFPA - 2)]) / (len(toein2FPA) - 41)
+    print('percent steps in range during toe-in 2: ' + str(inRange_t2))
+    inRange_t3 = 100 * len(pd.concat([toein3FPA.iloc[:80], toein3FPA.iloc[121:]])[(pd.concat([toein3FPA.iloc[:80], toein3FPA.iloc[121:]]).iloc[:, 2] < targetFPA + 2) & (pd.concat([toein3FPA.iloc[:80], toein3FPA.iloc[121:]]).iloc[:, 2] > targetFPA - 2)]) / (len(toein3FPA) - 41)
+    print('percent steps in range during toe-in 3: ' + str(inRange_t3))
+    inRange_t4 = 100 * len(pd.concat([toein4FPA.iloc[:80], toein4FPA.iloc[121:]])[(pd.concat([toein4FPA.iloc[:80], toein4FPA.iloc[121:]]).iloc[:, 2] < targetFPA + 2) & (pd.concat([toein4FPA.iloc[:80], toein4FPA.iloc[121:]]).iloc[:, 2] > targetFPA - 2)]) / (len(toein4FPA) - 41)
+    print('percent steps in range during toe-in 4: ' + str(inRange_t4))
+
+    # iii. catch trials
+    inRange_c1 = 100 * len(toein1FPA.iloc[80:121][(toein1FPA.iloc[80:121, 2] < targetFPA + 2) & (toein1FPA.iloc[80:121, 2] > targetFPA - 2)]) / 40
+    print('percent steps in range during catch 1: ' + str(inRange_c1)) 
+    inRange_c2 = 100 * len(toein2FPA.iloc[80:121][(toein2FPA.iloc[80:121, 2] < targetFPA + 2) & (toein2FPA.iloc[80:121, 2] > targetFPA - 2)]) / 40
+    print('percent steps in range during catch 2: ' + str(inRange_c2))
+    inRange_c3 = 100 * len(toein3FPA.iloc[80:121][(toein3FPA.iloc[80:121, 2] < targetFPA + 2) & (toein3FPA.iloc[80:121, 2] > targetFPA - 2)]) / 40
+    print('percent steps in range during catch 3: ' + str(inRange_c3))
+    inRange_c4 = 100 * len(toein4FPA.iloc[80:121][(toein4FPA.iloc[80:121, 2] < targetFPA + 2) & (toein4FPA.iloc[80:121, 2] > targetFPA - 2)]) / 40
+    print('percent steps in range during catch 4: ' + str(inRange_c4))
+
+    # iv. retention trial
+    inRange_ret = 100*len(retFPA[(retFPA.iloc[:,2]<targetFPA+2) & (retFPA.iloc[:,2]>targetFPA-2)])/len(retFPA)
+    print('percent steps in range during retention: ' + str(inRange_ret)) 
+
+    # v. store all
+    inRange_all = [inRange_nf, inRange_t1, inRange_t2, inRange_t3, inRange_t4, inRange_ret]
+    inRange_NFs = [inRange_nf, inRange_c1, inRange_c2, inRange_c3, inRange_c4, inRange_ret]
+    store_inRangePercent[subject-1] = inRange_all
+    store_inRangeNFPercent[subject-1] = inRange_NFs
+
+    # f. compute RMSE:
+    # i. nominal
+    RMSENF = np.sqrt(np.mean( (bFPA_deg-10-nfFPA.iloc[:,2])**2 ))
+    
+    # ii. toe-in trials
+    RMSET1 = np.sqrt(np.mean( (bFPA_deg-10-toein1FPA.iloc[:,2])**2 ))
+    RMSET2 = np.sqrt(np.mean( (bFPA_deg-10-toein2FPA.iloc[:,2])**2 ))
+    RMSET3 = np.sqrt(np.mean( (bFPA_deg-10-toein3FPA.iloc[:,2])**2 ))
+    RMSET4 = np.sqrt(np.mean( (bFPA_deg-10-toein4FPA.iloc[:,2])**2 ))
+    
+    # iii. catch trials
+    RMSEC1 = np.sqrt(np.mean( (bFPA_deg-10-toein1FPA.iloc[80:121,2])**2 ))
+    RMSEC2 = np.sqrt(np.mean( (bFPA_deg-10-toein2FPA.iloc[80:121,2])**2 ))
+    RMSEC3 = np.sqrt(np.mean( (bFPA_deg-10-toein3FPA.iloc[80:121,2])**2 ))
+    RMSEC4 = np.sqrt(np.mean( (bFPA_deg-10-toein4FPA.iloc[80:121,2])**2 ))
+    
+    # iv. retention
+    RMSER = np.sqrt(np.mean( (bFPA_deg-10-retFPA.iloc[:,2])**2 ))
+
+    # v. store all    
+    RMSE_all = [RMSENF, RMSET1, RMSET2, RMSET3, RMSET4, RMSER]
+    store_RMSE[subject-1] = RMSE_all
+
+    C_RMSE_all = [RMSENF, RMSEC1, RMSEC2, RMSEC3, RMSEC4, RMSER]
+    store_C_RMSE[subject-1] = C_RMSE_all
+
+    # g. compute responsiveness
+    store_resp_NF = calculate_responsiveness(nfFPA, targetFPA)
+    store_resp_RT1 = calculate_responsiveness(toein1FPA, targetFPA)
+    store_resp_RT2 = calculate_responsiveness(toein2FPA, targetFPA)
+    store_resp_RT3 = calculate_responsiveness(toein3FPA, targetFPA)
+    store_resp_RT4 = calculate_responsiveness(toein4FPA, targetFPA)
+    store_resp_RET = calculate_responsiveness(retFPA, targetFPA)
+
+    resp_all = [store_resp_NF, store_resp_RT1, store_resp_RT2, store_resp_RT3, store_resp_RT4, store_resp_RET]
+    store_resp[subject-1] = resp_all
+
+# 2. Plot group means and cumulative results
+SF_rows = np.where(feedbackCond_file.cond == 1)[0]
+TF_rows = np.where(feedbackCond_file.cond == 2)[0]
+NF_rows = np.where(feedbackCond_file.cond == 0)[0]
+
+# i. Accuracy over time
+x = np.arange(6)
+plt.figure(figsize=(6,6))
+plt.title('Accuracy over time')
+plt.plot(x-0.05, np.mean(store_inRangePercent[SF_rows], axis=0), '-o', color = '#0f4c5c', label = 'SF')
+plt.errorbar(x-0.05, np.mean(store_inRangePercent[SF_rows], axis=0), yerr=np.std(store_inRangePercent[SF_rows], axis=0), fmt='none', ecolor='#0f4c5c', capsize=5)
+
+plt.plot(x, np.mean(store_inRangePercent[TF_rows], axis=0), '-o', color = '#5f0f40', label = 'TF')
+plt.errorbar(x, np.mean(store_inRangePercent[TF_rows], axis=0), yerr=np.std(store_inRangePercent[TF_rows], axis=0), fmt='none', ecolor='#5f0f40', capsize=5)
+
+plt.plot(x+0.05, np.mean(store_inRangePercent[NF_rows], axis=0), '-o', color = '#e36414', label = 'NF')
+plt.errorbar(x+0.05, np.mean(store_inRangePercent[NF_rows], axis=0), yerr=np.std(store_inRangePercent[NF_rows], axis=0), fmt='none', ecolor='#e36414', capsize=5)
+
+plt.legend()
+plt.ylim([0,100])
+plt.ylabel('Steps within target range (%)')
+plt.show()
+
+# ii. RMSE over time
+plt.figure(figsize=(6,6))
+plt.title('RMSE over time')
+plt.plot(x-0.05, np.mean(store_RMSE[SF_rows], axis=0), '-o', color = '#0f4c5c', label = 'SF')
+plt.errorbar(x-0.05, np.mean(store_RMSE[SF_rows], axis=0), yerr=np.std(store_RMSE[SF_rows], axis=0), fmt='none', ecolor='#0f4c5c', capsize=5)
+
+plt.plot(x, np.mean(store_RMSE[TF_rows], axis=0), '-o', color = '#5f0f40', label = 'TF')
+plt.errorbar(x, np.mean(store_RMSE[TF_rows], axis=0), yerr=np.std(store_RMSE[TF_rows], axis=0), fmt='none', ecolor='#5f0f40', capsize=5)
+
+plt.plot(x+0.05, np.mean(store_RMSE[NF_rows], axis=0), '-o', color = '#e36414', label = 'NF')
+plt.errorbar(x+0.05, np.mean(store_RMSE[NF_rows], axis=0), yerr=np.std(store_RMSE[NF_rows], axis=0), fmt='none', ecolor='#e36414', capsize=5)
+
+plt.legend()
+plt.ylim([0,12])
+plt.ylabel('RMSE (deg)')
+plt.show()
+
+# iii. RMSE over time (catch trials)
+plt.figure(figsize=(6,6))
+plt.title('catch trial RMSEs')
+plt.plot(x-0.05, np.mean(store_C_RMSE[SF_rows], axis=0), '-o', color = '#0f4c5c', label = 'SF')
+plt.errorbar(x-0.05, np.mean(store_C_RMSE[SF_rows], axis=0), yerr=np.std(store_C_RMSE[SF_rows], axis=0), fmt='none', ecolor='#0f4c5c', capsize=5)
+
+plt.plot(x, np.mean(store_C_RMSE[TF_rows], axis=0), '-o', color = '#5f0f40', label = 'TF')
+plt.errorbar(x, np.mean(store_C_RMSE[TF_rows], axis=0), yerr=np.std(store_C_RMSE[TF_rows], axis=0), fmt='none', ecolor='#5f0f40', capsize=5)
+
+plt.plot(x+0.05, np.mean(store_C_RMSE[NF_rows], axis=0), '-o', color = '#e36414', label = 'NF')
+plt.errorbar(x+0.05, np.mean(store_C_RMSE[NF_rows], axis=0), yerr=np.std(store_C_RMSE[NF_rows], axis=0), fmt='none', ecolor='#e36414', capsize=5)
+plt.show()
+
+# iv. change in RMSE (NF vs. RT4)
+fig, ax = plt.subplots(figsize = (6,6))
+sf_data = 100*(store_RMSE[SF_rows,4] - store_RMSE[SF_rows,0])/store_RMSE[SF_rows,0]
+tf_data = 100*(store_RMSE[TF_rows,4] - store_RMSE[TF_rows,0])/store_RMSE[TF_rows,0]
+nf_data = 100*(store_RMSE[NF_rows,4] - store_RMSE[NF_rows,0])/store_RMSE[NF_rows,0]
+violin_parts = ax.violinplot([sf_data, tf_data, nf_data], 
+                             positions=[1, 2, 3], 
+                             showmeans=True, 
+                             showextrema=True, 
+                             showmedians=False)
+ax.set_title('Change in RMSE between NF and RT4')
+ax.set_ylabel('Relative change in RMSE')
+ax.set_xticks([1, 2, 3])
+ax.set_xticklabels(['SF', 'TF', 'NF'])
+
+for i, data in enumerate([sf_data, tf_data, nf_data], start=1):
+    ax.scatter(np.random.normal(i, 0.04, len(data)), data, alpha=0.3, s=15)
+plt.ylim([-85, 150])
+plt.show()
+
+# v. change in RMSE (NF vs RET)
+fig, ax = plt.subplots(figsize = (6,6))
+sf_data = 100*(store_RMSE[SF_rows,5] - store_RMSE[SF_rows,0])/store_RMSE[SF_rows,0]
+tf_data = 100*(store_RMSE[TF_rows,5] - store_RMSE[TF_rows,0])/store_RMSE[TF_rows,0]
+nf_data = 100*(store_RMSE[NF_rows,5] - store_RMSE[NF_rows,0])/store_RMSE[NF_rows,0]
+violin_parts = ax.violinplot([sf_data, tf_data, nf_data], 
+                             positions=[1, 2, 3], 
+                             showmeans=True, 
+                             showextrema=True, 
+                             showmedians=False)
+ax.set_title('Change in RMSE between NF and Retention')
+ax.set_ylabel('Relative change in RMSE')
+ax.set_xticks([1, 2, 3])
+ax.set_xticklabels(['SF', 'TF', 'NF'])
+
+for i, data in enumerate([sf_data, tf_data, nf_data], start=1):
+    ax.scatter(np.random.normal(i, 0.04, len(data)), data, alpha=0.3, s=15)
+plt.show()
+
+# vi. change in RMSE (catch trial)
+fig, ax = plt.subplots(figsize = (6,6))
+sf_data = 100*(store_C_RMSE[SF_rows,4] - store_RMSE[SF_rows,0])/store_RMSE[SF_rows,0]
+tf_data = 100*(store_C_RMSE[TF_rows,4] - store_RMSE[TF_rows,0])/store_RMSE[TF_rows,0]
+nf_data = 100*(store_C_RMSE[NF_rows,4] - store_RMSE[NF_rows,0])/store_RMSE[NF_rows,0]
+violin_parts = ax.violinplot([sf_data, tf_data, nf_data], 
+                             positions=[1, 2, 3], 
+                             showmeans=True, 
+                             showextrema=True, 
+                             showmedians=False)
+ax.set_title('Change in RMSE between NF and catch RT4')
+ax.set_ylabel('Relative change in RMSE')
+ax.set_xticks([1, 2, 3])
+ax.set_xticklabels(['SF', 'TF', 'NF'])
+
+for i, data in enumerate([sf_data, tf_data, nf_data], start=1):
+    ax.scatter(np.random.normal(i, 0.04, len(data)), data, alpha=0.3, s=15)
+plt.show()
